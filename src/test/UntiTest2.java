4fb11f647529dffcfc19c23f7942f558b6e73485
transformers24 <mohammad.amin@tekschool.us>
2024-07-13 02:32:49 -0400

    Update UntiTest2.java
    This is for Pull practice

--- src/test/UntiTest2.java
+++ src/test/UntiTest2.java
@@ -4,6 +4,7 @@ public class UntiTest2 {
 	
 	public static void main(String[] args) {
 		System.out.println("This is for Push practice");
+		System.out.printlin("This is for Pull practice");
 	}
 
 }
