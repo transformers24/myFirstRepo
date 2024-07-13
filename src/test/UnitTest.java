32258d954f866035f0a8282be678c94cab5950ff
daud <mohammad.amin@tekschool.us>
2024-07-13 01:52:51 -0400

    new code/method added

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -9,6 +9,12 @@ public class UnitTest {
 		int c = a + b;
 		
 		System.out.println(c);
+		System.out.println(testOne(10,5));
 	}
-
+	
+	public static int testOne (int a, int b) {
+		return a-b;
+	}
+	
+	
 }
